d65665e0a5da323ca976ecd2f1fedcf12be4c615
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-09-29 23:58:08 -0400

    Time: 1342 ms (13.96%) | Memory: 59.9 MB (5.35%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -2,5 +2,7 @@ from itertools import permutations
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
+        # create permutation list
         p = list(permutations(range(1,n+1)))
+        # return 'kth' element from permutation list
         return ''.join(str(i) for i in p[k-1])
