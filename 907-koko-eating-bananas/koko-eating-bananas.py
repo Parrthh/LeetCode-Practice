362e216a8d225ed5d8fe7c4576415136ae84648f
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-10-24 21:16:42 -0400

    Time: 149 ms (95.58%) | Memory: 17.8 MB (91.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,18 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # Binary search between speed 1 and max(piles)
-        left, right = 1, max(piles)
-        # Function to calculate the number of hours required
-        while left < right:
-            hours = 0
-            mid = (left + right) // 2
+        left, right = 1, max(piles)     # right is max possible eating speed of Koko
 
+        while left < right:
+            mid = (left + right) // 2
+            hours = 0
             for p in piles:
                 hours += math.ceil(p/mid)
-            
             if hours <= h:
                 right = mid
             else:
                 left = mid + 1
-        return right
-
+        return left
+                
