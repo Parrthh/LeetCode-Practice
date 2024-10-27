97ec7eb5cc671a6d852cd80ab251de312a1202a7
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-10-27 15:52:02 -0400

    Time: 3 ms (12.43%) | Memory: 16.6 MB (50.31%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0 or nums is None:
+            return 0
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        return max(self.max_rob(nums[:-1]), self.max_rob(nums[1: ]))
+
+    def max_rob(self,nums):
+        t1 , t2 = 0, 0
+        for current in nums:
+            temp = t1
+            t1 = max(current + t2, t1)
+            t2 = temp
+        return t1
+            
+
+            
+            
+
+            
+        
+        
