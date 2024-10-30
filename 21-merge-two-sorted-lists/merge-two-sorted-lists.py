e3adc5480c5c480f0149aeab45715c67b85a123b
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-10-30 13:31:58 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (18.74%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,34 +5,33 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Placeholder node for the result list
+        # Creating a dummy node to act as the head of the merged list
+        # Because when we are creating an object here
+        # The value of the object is 0
+        # That is why when we are returning dummy, we next to use dummy.next to avoid 0
         dummy = ListNode()
-        # Tail node to perform the merge process
-        tail = dummy
+        
+        # Pointer temp keep tracks of the last node in the merged list
+        temp = dummy
 
-        # While loop to iterate over both the lists
-        # It also checks if anyone of the list is not null
-        # If it is then we directly go the outside if case to add values
-        # Directly in the result list
-        while list1 and list2:    
-            # Comparing the curr nodes of both the list
+        # While loop the compare the current nodes of list1 and list2
+        # Anyone which is smaller will be stored in dummy
+        while list1 and list2:
             if list1.val < list2.val:
-                # if curr of l1 is small compared to curr of l2
-                tail.next = list1
+                temp.next = list1
                 list1 = list1.next
             else:
-                # if curr of l2 is small compared to l1
-                tail.next = list2
+                temp.next = list2
                 list2 = list2.next
-            tail = tail.next
-        
-        # There can be a case wherein anyone of the list might be longer than the other
-        # So we directly store it into the result list
+            temp = temp.next
+        # There can be a test case, where one list can have more elements compared to another
         if list1:
-            tail.next = list1
+            temp.next = list1
         elif list2:
-            tail.next = list2
-        
-        # Returning our final list
+            temp.next = list2
+        # As we have our dummy node, we would want to skip that
+        # Therefore, we use dummy.next
         return dummy.next
-        
+
+
+
