b9fe22ad9a8f04c30139e1d357b0996cb6f7f6f3
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-11-05 17:21:26 -0500

    Time: 29 ms (90.68%) | Memory: 16.6 MB (30.51%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        
+        cache = [0] * (n + 1)
+        cache[1] = 1
+        for i in range(2, n +1):
+            cache[i] = cache[i - 1] + cache[i - 2]
+        return cache[n]
+
+
+"""
+TIME COMPLEXITY: O(N) -> bottom-up approach
+1. Each number, starting at 2 up to and incuding N, is visited computed and then sorted for O(1)
+access later on
+
+
+SPACE COMPLEXITY: O(n)
+1. The size of the data structure is proportional to N
+
+
+"""
