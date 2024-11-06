814635fe54b58674597e6df3036c4eb5644e95d9
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-11-06 12:15:15 -0500

    Time: 143 ms (80.26%) | Memory: 17.7 MB (85.60%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -13,3 +13,12 @@ class Solution:
                 left = mid + 1
         return left
                 
+"""
+TIME COMPLEXITY: O(n log(max(piles)))
+1. Since each step in the binary search takes O(n) time to compute the required hrs for the current speed
+2. Binary search alone has a time complexity of O(log(max(piles))), as it depends on the maximum value in piles
+3. Therefore, the total time complexity computation is O(n log(max(piles)))
+
+SPACE COMPLEXITY:
+1. The space complexity is O(1) since we only use a few extra variables and no additional data structure
+"""
