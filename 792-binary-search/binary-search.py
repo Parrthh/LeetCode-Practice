05e04d80e199dffb25f69d6320273ca4f2a9c04b
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-11-06 11:58:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.11%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -13,4 +13,15 @@ class Solution:
                 left = mid + 1
         return -1 
 
-        
+"""
+TIME COMPLEXITY: O(log n)
+1. After each iteration, the search range is halved
+2. Thus, the number of iterated required to reduce the search range to a single element is logarithmic
+
+SPACE COMPLEXITY: O(1)
+1. The algorithm uses only a few extra variables, which all occupy constant time O(1)
+2. No additional data structures or recursion stacks are used, so the space complexity is minimal
+3. Therefore, the overall space complexity is O(1)
+
+
+"""
