3699ae269a137b1e70b57f7e78a8896041ae3480
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2024-11-14 13:53:30 -0500

    Time: 274 ms (65.48%) | Memory: 16.8 MB (22.79%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,23 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        res_length = 0
+        res = ""          # Stores the longest palindromic substring found
+        res_length = 0    # Stores the length of the longest palindromic substring
+
+        # Iterate through each character in the string as the center
         for i in range(len(s)):
-            # Check odd length palindromes
-            left, right = i ,i
+            # Check for odd-length palindromes
+            left, right = i, i
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                # If the current palindrome is longer than the previous longest
                 if (right - left + 1) > res_length:
-                    res = s[left: right + 1]
-                    res_length = right - left + 1
-                left -= 1
-                right += 1
-            # Even length:
+                    res = s[left: right + 1]     # Update the result
+                    res_length = right - left + 1  # Update the length
+                left -= 1  # Expand to the left
+                right += 1 # Expand to the right
+
+            # Check for even-length palindromes
             left, right = i, i + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
+                # If the current palindrome is longer than the previous longest
                 if (right - left + 1) > res_length:
-                    res = s[left: right + 1]
-                    res_length = right - left + 1
-                left -= 1
-                right += 1
+                    res = s[left: right + 1]     # Update the result
+                    res_length = right - left + 1  # Update the length
+                left -= 1  # Expand to the left
+                right += 1 # Expand to the right
+
         return res
+
         
