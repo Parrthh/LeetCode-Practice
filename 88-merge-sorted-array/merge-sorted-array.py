1dd7cf1e1135129b636bbb2fb57f2a502fc89cd6
Parth Kharkar <102367787+Parrthh@users.noreply.github.com>
2025-01-05 19:34:39 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (28.62%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,26 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
-        p = m + n - 1
-
-        while p1 >= 0 and p2 >= 0:
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        while p1 >=0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
-                p1-=1
+                p1 -=1 
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
             p -= 1
         
-        
-
-        while p2>= 0:
+        while p2 >= 0:
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
 
+
+"""
+TIME COMPLEXITY: O(m + n)
+1. As we iterate through both the arrays once
+
+SPACE COMPLEXITY: O(1)
+1. As the operation is in-place
+"""
         
 
         
